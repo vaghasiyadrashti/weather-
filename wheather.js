988a910f21d273b036abaa7e255b5b3e1bcc0664
Drashti <drashtivaghasiya39@gmail.com>
2024-02-10 21:49:35 +0530

    setup weather htmml and js file

---
+++ wheather.js
@@ -0,0 +1,45 @@
+// API FETCH
+
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3a3c2c7377msh603d0bd2048fcc2p13384cjsn043cc74bdb7c",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+
+const getWheather = (city) => {
+  cityName.innerHTML = city;
+  fetch(
+    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+
+      cloud_pct.innerHTML = response.cloud_pct;
+      temp.innerHTML = response.temp;
+      temp2.innerHTML = response.temp;
+      feels_like.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      humidity2.innerHTML = response.humidity;
+      min_temp.innerHTML = response.min_temp;
+      max_temp.innerHTML = response.max_temp;
+      wind_speed.innerHTML = response.wind_speed;
+      wind_speed2.innerHTML = response.wind_speed;
+      // wind_degrees.innerHTML = response.wind_degrees;
+      sunrise.innerHTML = response.sunrise;
+      sunset.innerHTML = response.sunset;
+    })
+
+    .catch((err) => console.error(err));
+};
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  getWheather(city.value);
+});
+
+getWheather("Gujarat");
